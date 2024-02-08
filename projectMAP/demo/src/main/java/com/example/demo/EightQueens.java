709cc876e0ec9715485073e0cf4b798367584468
gabriel <grecualexandrugabriel@yahoo.com>
2024-02-08 02:52:20 +0200

    check where is the queen

--- projectMAP/demo/src/main/java/com/example/demo/EightQueens.java
+++ projectMAP/demo/src/main/java/com/example/demo/EightQueens.java
@@ -4,16 +4,16 @@ public class EightQueens {
     final int N = 8;
     int[][] board = new int[N][N];
 
-    // Metoda pentru a verifica dacă o regină poate fi plasată în poziția x
+    // Metoda pentru a verifica daca o regina poate fi plasata în pozitia x
     boolean isSafe(int row, int col) {
         int i, j;
 
-        // Verifică rândul pe partea stângă
+        // Verifica randul pe partea stanga
         for (i = 0; i < col; i++)
             if (board[row][i] == 1)
                 return false;
 
-        // Verifică partea superioară a diagonalei stânga
+        // Verifica partea superioara a diagonalei stanga
         for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
             if (board[i][j] == 1)
                 return false;
